b7af8fd52ef214af2e230b9ed4ab139d14d3c5e9
Massimo Santini <massimo.santini@gmail.com>
2016-11-07 21:40:26 +0100

    Removing stale dir

--- src/sf/zipgettext.py
+++ src/sf/zipgettext.py
@@ -1,19 +1,18 @@
-from errno import ENOENT
 from gettext import GNUTranslations, NullTranslations
 from io import BytesIO
 from locale import getdefaultlocale
 from os.path import isdir, join, dirname
 from zipfile import ZipFile
 
-PACKAGE_PATH = dirname( dirname( __file__ ) )
+PACKAGE_PATH = dirname(dirname( __file__ ))
 
-def translation( lang ):
+def translation(lang):
 	try:
-		with ZipFile( PACKAGE_PATH ) as f:
-			mo = f.read( 'sf/mos/{0}.mo'.format( lang ) )
+		with ZipFile(PACKAGE_PATH) as f:
+			mo = f.read('sf/mos/{0}.mo'.format(lang))
 	except KeyError:
 		return NullTranslations()
 	else:
-		return GNUTranslations( BytesIO( mo ) )
+		return GNUTranslations(BytesIO(mo))
 
-DEFAULT_GETTEXT = translation( getdefaultlocale()[0][:2] ).gettext
+DEFAULT_GETTEXT = translation(getdefaultlocale()[0][:2]).gettext
