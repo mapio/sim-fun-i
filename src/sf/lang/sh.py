6b18adb8eea9076403700ca985b7f23ed2c8fd28
Massimo Santini <massimo.santini@gmail.com>
2016-12-01 22:14:15 +0100

    Just sh is enough

--- src/sf/lang/sh.py
+++ src/sf/lang/sh.py
@@ -8,7 +8,7 @@ class ShSolution(Solution):
     MAIN_SOURCE_RE = re.compile(r'.*')
     def __init__(self, path):
         super(ShSolution, self).__init__(path)
-        if self.main_source: self.run_command = ['bash', self.main_source[0]]
+        if self.main_source: self.run_command = ['sh', self.main_source[0]]
     def compile(self):
         return Result(None,None,None,0)
     def is_compiled(self):
