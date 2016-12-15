f63489801aaa2b5ecb0ff11a04eb5ac5c1bad825
Massimo Santini <massimo.santini@gmail.com>
2016-12-15 13:38:27 +0100

    Better handling of multiple class solutions

--- src/sf/lang/java.py
+++ src/sf/lang/java.py
@@ -6,13 +6,20 @@ from sf.solution import Solution, execute
 class JavaSolution(Solution):
     SOURCES_GLOB = '*.java'
     MAIN_SOURCE_RE = re.compile(r'public\s+static\s+void\s+main', re.MULTILINE)
+    MAIN_PUBLIC_CLASS_RE = re.compile(r'public\s+class\s+(\w+)', re.MULTILINE)
     MAIN_CLASS_RE = re.compile(r'class\s+(\w+)', re.MULTILINE)
     def __init__(self, path):
         super(JavaSolution, self).__init__(path)
         if self.main_source:
-            match = JavaSolution.MAIN_CLASS_RE.search(self.main_source[1])
-            if match: self.main_class = match.group(1)
-            else: self.main_class, self.main_source = None, None
+            match = JavaSolution.MAIN_PUBLIC_CLASS_RE.search(self.main_source[1])
+            if match:
+                self.main_class = match.group(1)
+            else:
+                match = JavaSolution.MAIN_CLASS_RE.search(self.main_source[1])
+                if match:
+                    self.main_class = match.group(1)
+                else:
+                    self.main_class, self.main_source = None, None
             self.run_command = ['java', '-Duser.language=ROOT', self.main_class]
     def compile(self):
         return execute(['javac'], args = self.sources, cwd = self.path)
