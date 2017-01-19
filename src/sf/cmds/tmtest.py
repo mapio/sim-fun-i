d4eb219886fa45f20c3a5ba6086d4427621c473d
Massimo Santini <massimo.santini@gmail.com>
2017-01-20 00:07:20 +0100

    Fixing spacing

--- src/sf/cmds/tmtest.py
+++ src/sf/cmds/tmtest.py
@@ -14,7 +14,7 @@ def main():
 	parser.add_argument('--uploads', '-u', help = 'The uploads directory.', required = True)
 	parser.add_argument('--uid', '-U', help = 'The UID to test (default: all).')
 	parser.add_argument('--timestamp', '-T', help = 'The timestamp of the upload to test (default: latest).')
-	parser.add_argument('--no-clean', '-n', default = False, help = 'Whether to clean the untarred upload directory first.', action='store_true')
+	parser.add_argument('--no-clean', '-n', default = False, help = 'Whether to clean the untarred upload directory first.', action = 'store_true')
 	parser.add_argument('--results', '-r', help = 'The file where to store the json summary of the results', type = FileType( 'wb' ) )
 	args = parser.parse_args()
 
