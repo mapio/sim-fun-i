ae1cf3484a434675717ab1d7c1db6ef09b93b5e4
Massimo Santini <massimo.santini@gmail.com>
2017-02-10 21:11:13 +0100

    Added an option to restrict the UIDs of tmtest; closes #5

--- src/sf/__init__.py
+++ src/sf/__init__.py
@@ -1,7 +1,7 @@
 DEFAULT_ENCODING = 'utf-8'
 MAX_BYTES_READ = 1048576
 TEST_TIMEOUT = 1
-VERSION = '0.2.0'
+VERSION = '0.2.1'
 
 class WronglyEncodedFile(UnicodeError):
     pass
--- src/sf/cmds/tmtest.py
+++ src/sf/cmds/tmtest.py
@@ -5,6 +5,7 @@ from sys import argv, stderr, exit
 
 from colorama import Fore, Style
 
+from tm.mkconf import read_uids
 from sf.tmdata import TristoMietitoreConfig, TristoMietitoreUploads, tmtest
 from sf.scanner import Scanner
 
@@ -12,7 +13,9 @@ def main():
 	parser = ArgumentParser(prog = 'sf tmtest')
 	parser.add_argument('--config', '-c', help = 'The path of the Tristo Mietitore config file.', required = True)
 	parser.add_argument('--uploads', '-u', help = 'The uploads directory.', required = True)
-	parser.add_argument('--uid', '-U', help = 'The UID to test (default: all).')
+	group = parser.add_mutually_exclusive_group()
+	group.add_argument('--uid', '-U', help = 'The UID to test (default: all).')
+	group.add_argument('--uids-file', '-F', help = 'The UIDs to test, as a tab-separated file (default: all).')
 	parser.add_argument('--timestamp', '-T', help = 'The timestamp of the upload to test (default: latest).')
 	parser.add_argument('--no-clean', '-n', default = False, help = 'Whether to clean the untarred upload directory first.', action = 'store_true')
 	parser.add_argument('--results', '-r', help = 'The file where to store the json summary of the results', type = FileType( 'wb' ) )
@@ -20,8 +23,16 @@ def main():
 
 	config = TristoMietitoreConfig(args.config)
 	uploads = TristoMietitoreUploads(args.uploads)
-	uids = [args.uid] if args.uid else uploads.uids()
+
+	if args.uid:
+		uids = [args.uid]
+	elif args.uids_file:
+		uids = read_uids(args.uids_file).keys()
+	else:
+		uids = uploads.uids()
+
 	for uid in uids: tmtest(config, uploads, uid, args.timestamp, not args.no_clean)
+
 	if args.results:
-		args.results.write( Scanner(args.uploads).scan().sort().tojson() )
+		args.results.write(Scanner(args.uploads).scan().sort().tojson(lambda _: _['signature']['uid'] in frozenset(uids)))
 		args.results.close()
