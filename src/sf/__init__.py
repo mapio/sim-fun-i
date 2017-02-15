2ed106f6ca2d512f6666c8c3135f885265f6c310
Massimo Santini <massimo.santini@gmail.com>
2017-02-15 14:00:18 +0100

    Now it is possible to specify the timeout via the SIMFUNI_TIMEOUT env var; closes #6

--- src/sf/__init__.py
+++ src/sf/__init__.py
@@ -1,7 +1,9 @@
+from os import environ
+
 DEFAULT_ENCODING = 'utf-8'
 MAX_BYTES_READ = 1048576
-TEST_TIMEOUT = 1
-VERSION = '0.2.2'
+TEST_TIMEOUT = int(environ.get('SIMFUNI_TIMEOUT', 1))
+VERSION = '0.2.3'
 
 class WronglyEncodedFile(UnicodeError):
     pass
