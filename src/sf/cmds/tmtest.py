79b0d26240b576649b86f443a936fb0de1d5c96a
Massimo Santini <massimo.santini@gmail.com>
2017-06-13 16:04:20 +0200

    Fixed spacing

--- src/sf/cmds/tmtest.py
+++ src/sf/cmds/tmtest.py
@@ -18,7 +18,7 @@ def main():
 	group.add_argument('--uids-file', '-F', help = 'The UIDs to test, as a tab-separated file (default: all).')
 	parser.add_argument('--timestamp', '-T', help = 'The timestamp of the upload to test (default: latest).')
 	parser.add_argument('--no-clean', '-n', default = False, help = 'Whether to clean the untarred upload directory first.', action = 'store_true')
-	parser.add_argument('--results', '-r', help = 'The file where to store the json summary of the results', type = FileType( 'wb' ) )
+	parser.add_argument('--results', '-r', help = 'The file where to store the json summary of the results', type = FileType('wb') )
 	args = parser.parse_args()
 
 	config = TristoMietitoreConfig(args.config)
