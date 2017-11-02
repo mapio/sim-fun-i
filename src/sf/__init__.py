68eb50519c9216b3df18f43731058a661063ee7f
Massimo Santini <massimo.santini@gmail.com>
2017-11-02 11:26:13 +0100

    Bumbed release

--- src/sf/__init__.py
+++ src/sf/__init__.py
@@ -3,7 +3,7 @@ from os import environ
 DEFAULT_ENCODING = 'utf-8'
 MAX_BYTES_READ = 1048576
 TEST_TIMEOUT = int(environ.get('SIMFUNI_TIMEOUT', 1))
-VERSION = '0.3.1-beta'
+VERSION = '0.4.0'
 
 class WronglyEncodedFile(UnicodeError):
     pass
